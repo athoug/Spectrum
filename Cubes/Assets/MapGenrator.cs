0d9697a21b436dec87c315ac2fc71db8cae82a35
rehaf <ra0633a@american.edu>
2017-01-22 09:09:24 -0500

    changes

---
+++ Cubes/Assets/MapGenrator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenrator : MonoBehaviour {
+
+
+	public Transform tilePrefab;
+	public Vector2 mapSize;
+	[Range(0,1)]
+	public float outline;
+
+	public void GenrateMap(){
+
+		for( int x = 0; x< mapSize.x; x++){
+			for( int y = 0; y< mapSize.y ;y++){
+
+				Vector3 TilePos = new Vector3(-mapSize.x/2 + 0.5f + x, 1, -mapSize.y/2 + 0.5f + y);
+				Transform newTile = Instantiate( tilePrefab, TilePos, Quaternion.identity) as Transform;
+				newTile.localScale = Vector3.one * (1- outline);
+			}
+			
+		}
+
+	}
+	// Use this for initialization
+	void Start () {
+		GenrateMap();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Cubes/Assets/Scripts/CountDown.cs
+++ Cubes/Assets/Scripts/CountDown.cs
@@ -7,7 +7,12 @@ public class CountDown : MonoBehaviour {
 
 	public float timeLeft = 300.0f;
 	public Text text;
+	Death endScene;
 
+	void Start(){
+
+	endScene = FindObjectOfType<Death>();
+	}
 
 	// Update is called once per frame
 	void Update ()
@@ -16,9 +21,11 @@ public class CountDown : MonoBehaviour {
 		if (timeLeft < 0) {
 			// #TODO MAKE AN ACTUAL GAMEOVER PAGE
 			text.text = "Game over";
+			endScene.DeathAwaits();
+
 		} else {
 			timeLeft -= Time.deltaTime;
-			text.text = "Time: " + Mathf.Round (timeLeft);
+			text.text = "Time: " + Mathf.Round (timeLeft) +"\nTill the light is gone...";
 		}
 	}
 }
